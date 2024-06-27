3fd7d58943e22c60e32bfd9779e58f125d924b78
Knut Olav Løite <koloite@gmail.com>
2024-06-27 07:27:39 +0200

    chore: create test app

---
+++ src/main/java/com/google/cloud/example/JdbcApp.java
@@ -0,0 +1,20 @@
+package com.google.cloud.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JdbcApp {
+  
+  public static void main(String[] args) throws SQLException {
+    try (Connection connection = DriverManager.getConnection("jdbc:cloudspanner:/projects/emulator-project/instances/test-instance/databases/my-database?autoConfigEmulator=true")) {
+      try (ResultSet resultSet = connection.createStatement().executeQuery("select 'Hello world!'")) {
+        while (resultSet.next()) {
+          System.out.println(resultSet.getString(1));
+        }
+      }
+    }
+  }
+
+}
